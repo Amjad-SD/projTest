7409920d3d63b896581f22fa349aa88d9a16585f
Amjad <amjad.88sd@gmail.com>
2022-11-17 12:35:34 +0300

    hi2

--- testind/dart_application_1/bin/dart_application_1.dart
+++ testind/dart_application_1/bin/dart_application_1.dart
@@ -3,3 +3,4 @@ import 'package:dart_application_1/dart_application_1.dart' as dart_application_
 void main(List<String> arguments) {
   print('Hello world: ${dart_application_1.calculate()}!');
 }
+//mklmklm
